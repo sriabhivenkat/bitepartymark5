9c6cbfac42cf5c1ad73518e5fde4ba02fdea58dc
sriabhivenkat <sriabhi01@gmail.com>
2021-01-25 22:07:08 -0600

    slider issue

--- screens/FlavorTestViewController.js
+++ screens/FlavorTestViewController.js
@@ -1,13 +1,13 @@
-import React, { useContext, useEffect, useState,} from 'react';
+import React, { useContext, useEffect, useState, useRef} from 'react';
 import { View, Image, StyleSheet,StatusBar, Animated, TouchableWithoutFeedback} from 'react-native';
 import {Icon} from 'react-native-elements';
 import {Text} from 'galio-framework';
+import {RadioButton} from 'react-native-paper'
 import firestore from "@react-native-firebase/firestore";
 import { AuthContext } from "../navigation/AuthProvider.js";
 import LinearGradient from "react-native-linear-gradient";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import TouchableScale from 'react-native-touchable-scale';
-import { PixelRatio } from 'react-native';
 
 
 const FlavorTestViewController = (props) => {
@@ -16,64 +16,78 @@ const FlavorTestViewController = (props) => {
     const [message, setMessage] = useState("Find out what you're into.");
     const [buttonText, setButtonText] = useState("Let's go!")
     const [stateval, setStateVal] = useState(false);
-    
-    function fadeIn() {
-        Animated.timing(opacity, {
+    const [value, setValue] = React.useState('first');
+    const introOpacity = useRef(new Animated.Value(0)).current
+    const textOpacity = useRef(new Animated.Value(0)).current
+
+
+    const fadeInIntro = Animated.timing(introOpacity,
+        {
             toValue: 1,
-            duration: 3000,
+            duration: 1500,
             useNativeDriver: true
-        }).start()
-        
-    }
+        }
+    )
 
-    function fadeOut() {
-        Animated.timing(opacity, {
+    const fadeOutIntro = Animated.timing(introOpacity,
+        {
             toValue: 0,
-            duration: 3000,
+            duration: 1500,
             useNativeDriver: true
-        }).start();
+        }
+    )
 
+    const fadeInText = Animated.timing(textOpacity,
+        {
+            toValue: 1,
+            duration: 1500,
+            useNativeDriver: true
+        }
+    )
+
+    // fade in on mount
+    useEffect(() => {
+        fadeInIntro.start()
+      }, [])
+
+
+    const handleTransition = () => {
+        fadeOutIntro.start()
+        fadeInText.start()
         setStateVal(true);
-    }
-
-    
+    }    
     return(
         <View style={styles.container}>
             <Animated.View
-                style={[
-                    styles.fadingContainer,
-                    {
-                        opacity: opacity,
-                    }
-                ]}
+                style={{opacity: introOpacity, marginTop: "50%"}}
             >
                 <Text numberOfLines={1} style={styles.fadingText}>{message}</Text>
                 <TouchableOpacity
                 activeOpacity={0.9}
-                onPress={fadeOut}
-                style={{width: "80%", height: '30%', marginLeft: "10%", justifyContent: "center"}}
+                onPress={handleTransition}
+                style={{width: "80%", height: '30%', marginLeft: "10%", justifyContent: "center",}}
                 >
                     <LinearGradient
                         start={{x:0, y:0}}
                         end={{x:1, y:0}}
                         colors={["#ee0979","#f76f6d",'#ff6a00']}
-                        style={{height: "100%", justifyContent: "center", alignItems: "center", borderRadius: 15}}>
+                        style={{height: "100%", justifyContent: "center", alignItems: "center", borderRadius: 15, }}>
                         <Text style={{color: "white", fontFamily: "PingFangHK-Medium", fontSize: 17}}>Let's go!</Text>
                     </LinearGradient>
                 </TouchableOpacity>
             </Animated.View>
-            {stateval==true && 
-                <Animated.View
-                    style={[
-                        styles.fadingContainer1,
-                        {
-                            opacity: fadeIn,
-                        }
-                    ]}
-                >
-                    <Text p>Suck my dick</Text>
-                </Animated.View>
-            }
+            <Animated.View
+                pointerEvents="none"
+                style={[{opacity: textOpacity, bottom: 400}]}
+            >
+                <Text numberOfLines={1} style={styles.fadingText}>Rate your sweet tooth</Text>
+                <View>
+                    <RadioButton.Group onValueChange={value => setValue(value)} value={value}>
+                        <RadioButton.Item label="First item" value="first" />
+                        <RadioButton.Item label="Second item" value="second" />
+                    </RadioButton.Group>
+                </View>
+            </Animated.View>
         </View>
     );
 }
