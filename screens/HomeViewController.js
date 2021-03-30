b64d8699c0a56ee8a08d29a8282af1d7ee0128d7
arvind-balaji <arvindb02@gmail.com>
2021-03-30 14:57:37 -0500

    Horizontal Cards

--- screens/HomeViewController.js
+++ screens/HomeViewController.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useContext } from "react";
-import { StyleSheet, FlatList, StatusBar } from "react-native";
+import { StyleSheet, FlatList, StatusBar, Dimensions, View } from "react-native";
 import { AuthContext } from "../navigation/AuthProvider.js";
 import { Text } from "galio-framework";
 import firestore from "@react-native-firebase/firestore";
@@ -58,25 +58,60 @@ const HomeViewController = ({ navigation }) => {
       <Text h2 style={[styles.title, { marginTop: 30 }]}>
         Parties
       </Text>
-      {invites
+      <View>
+      <FlatList
+        data={invites.filter((item) => item.status == "accepted")}
+        style={{ paddingTop: 5}}
+        snapToInterval={Dimensions.get('window').width}
+        horizontal
+        // persistentScrollbar
+        // disableIntervalMomentum
+        decelerationRate="fast"
+        indicatorStyle="black"
+        renderItem={({item}) => (
+          <PartyCard
+            // id={docID}
+            invite={item}
+            onPress={(id) =>
+              navigation.navigate("DuosPartyScreen", {
+                partyID: id,
+              })
+            }
+          />
+          // <Text>Foo</Text>
+        )}
+        keyExtractor={(item) => item.docId}
+      />
+      </View>
+     
+      {/* {invites
         .filter((item) => item.status == "accepted")
         .map(({ docID }) => (
           <PartyCard key={docID} id={docID} onPress={() =>  navigation.navigate("DuosPartyScreen", {
             partyID: docID,
           }) }/>
-        ))}
+        ))} */}
 
       <Text h3 style={[styles.title, styles.subtitle]}>
         Pending
       </Text>
+      <View>
       <FlatList
         data={invites.filter((item) => item.status == "pending")}
-        style={{ paddingTop: 5 }}
+        style={{ paddingTop: 5}}
+        horizontal
+        snapToInterval={Dimensions.get('window').width}
+        indicatorStyle="black"
+        decelerationRate="fast"
         renderItem={({ item }) => (
           <InviteCard invite={item} onAccept={handleAccept} />
         )}
         keyExtractor={(item) => item.id}
       />
+            {/* <Text>Foo</Text> */}
+
+      </View>
+     
     </SafeAreaView>
   );
 };
