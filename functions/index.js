b52f41268ae98701f0f8704542d56775322ca13d
arvind-balaji <arvindb02@gmail.com>
2021-03-31 13:18:49 -0500

    Update cloud functions

--- functions/index.js
+++ functions/index.js
@@ -17,7 +17,6 @@ exports.sendInviteNotification = functions.firestore
         notification: {
           title: "You have new party invite!",
           body: `Join ${inviter}'s party`,
-          // icon: follower.photoURL
         },
       };
 
@@ -28,5 +27,57 @@ exports.sendInviteNotification = functions.firestore
     }
     console.log("triggered!");
 
-    console.log({ userId, data });
+    // console.log({ userId, data });
+  });
+
+exports.resolveParty = functions.firestore
+  .document("Parties/{partyID}/members/{memberID}")
+  .onWrite(async (change, context) => {
+    const { partyID } = context.params;
+    try {
+      const membersList = (
+        await db.collection("Parties").doc(partyID).collection("members").get()
+      ).docs.map((x) => x.data());
+
+      const isComplete = membersList
+        .filter(({ status }) => status != "rejected")
+        .every(({ status }) => status == "complete");
+
+      if (isComplete) {
+        const { restaurants } = (
+          await db.collection("Parties").doc(partyID).get()
+        ).data();
+
+        const matchedRestaurant = restaurants
+          .sort(() => 0.5 - Math.random())
+          .sort((a, b) => b.matches - a.matches)[0];
+
+        console.log({ membersList });
+
+        const userPromises = membersList.map((member) =>
+          db.collection("Users").doc(member.uidvalue).get()
+        );
+
+        var tokens = (await Promise.all(userPromises)).flatMap(
+          (item) => item.data().tokens
+        );
+
+        await db.collection("Parties").doc(partyID).update({
+          winner: matchedRestaurant,
+        });
+
+        const payload = {
+          notification: {
+            title: "Join the party!",
+            body: `${matchedRestaurant.name} was selected`,
+            // icon: follower.photoURL
+          },
+        };
+
+        await admin.messaging().sendToDevice(tokens, payload);
+      }
+    } catch (err) {
+      console.log("Error getting documents", err);
+      throw err;
+    }
   });
