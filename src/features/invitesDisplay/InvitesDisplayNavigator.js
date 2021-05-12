5dac4e69a6f3b523d4b9113f534fb61e3df5f003
sriabhivenkat <sriabhi01@gmail.com>
2021-05-12 12:48:00 -0500

    Forhead Changes

--- src/features/invitesDisplay/InvitesDisplayNavigator.js
+++ src/features/invitesDisplay/InvitesDisplayNavigator.js
@@ -1,32 +1,59 @@
 import React from "react";
-import { Image } from "react-native";
+import { Image, Dimensions } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import InvitesDisplay from "./InvitesDisplay";
 
 const Stack = createStackNavigator();
 
-const LogoTitle = () => (
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const LogoTitle1 = () => (
   <Image
-    style={{ width: 100, height: 75, alignItems: "center" }}
+    style={{ width: 100, height: 70, alignItems: "center" }}
+    source={require("assets/images/headerlogo.png")}
+  />
+);
+
+const LogoTitle2 = () => (
+  <Image
+    style={{ width: 75, height: 50, alignItems: "center" }}
     source={require("assets/images/headerlogo.png")}
   />
 );
 
 export default () => (
   <Stack.Navigator>
-    <Stack.Screen
-      name="invitesDisplay"
-      component={InvitesDisplay}
-      options={{
-        title: "Invites",
-        headerShown: true,
-        headerTitle: (props) => <LogoTitle {...props} />,
-        headerStyle: {
-          height: 120,
-        },
-      }}
-    />
+    {windowHeight === 667 &&
+      <Stack.Screen
+        name="invitesDisplay"
+        component={InvitesDisplay}
+        options={{
+          title: "Invites",
+          headerShown: true,
+          headerTitle: (props) => <LogoTitle2 {...props} />,
+          headerStyle: {
+            height: 80,
+          },
+        }}
+      />
+    }
+
+    {windowHeight != 667 &&
+      <Stack.Screen
+        name="invitesDisplay"
+        component={InvitesDisplay}
+        options={{
+          title: "Invites",
+          headerShown: true,
+          headerTitle: (props) => <LogoTitle1 {...props} />,
+          headerStyle: {
+            height: 120,
+          },
+        }}
+      />
+    }
+
   </Stack.Navigator>
 );
-
