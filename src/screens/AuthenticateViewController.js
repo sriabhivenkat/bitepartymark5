71c8285d6ac2ae81aa7c5a606aa6730fdb407d9e
arvind-balaji <arvindb02@gmail.com>
2021-05-12 21:17:32 -0500

    Login Changes

--- src/screens/AuthenticateViewController.js
+++ src/screens/AuthenticateViewController.js
@@ -1,124 +1,152 @@
-import React, { useState } from 'react';
-import { View, Image, StyleSheet, ImageBackground, Keyboard, KeyboardAvoidingView, TouchableWithoutFeedback, SafeAreaView, } from 'react-native';
-import { Text, Input } from 'galio-framework';
+import React, { useState } from "react";
+import {
+  View,
+  Image,
+  StyleSheet,
+  ImageBackground,
+  Keyboard,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  SafeAreaView,
+} from "react-native";
+import { Text, Input } from "galio-framework";
 import LinearGradient from "react-native-linear-gradient";
-import { Headline, TextInput, Button } from 'react-native-paper';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { useContext } from 'react';
-import { AuthContext } from '../navigation/AuthProvider.js';
-import { GradientButton } from '../components';
-import { Card } from 'react-native-paper';
-import { Alert, Dimensions } from 'react-native';
+import { Headline, TextInput, Button } from "react-native-paper";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useContext } from "react";
+import { AuthContext } from "../navigation/AuthProvider.js";
+import { GradientButton } from "../components";
+import { Card } from "react-native-paper";
+import { Alert, Dimensions } from "react-native";
 
 const AuthenticateViewController = () => {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
 
-    const windowWidth = Dimensions.get('window').width;
-    const windowHeight = Dimensions.get('window').height;
+  const windowWidth = Dimensions.get("window").width;
+  const windowHeight = Dimensions.get("window").height;
 
-    const { login } = useContext(AuthContext);
-    return (
-
-        <TouchableWithoutFeedback
-            accessible={false}
-            onPress={() => Keyboard.dismiss()}>
-            <KeyboardAvoidingView
-                behavior="padding"
-                style={styles.container}
+  const { login } = useContext(AuthContext);
+  return (
+    <TouchableWithoutFeedback
+      accessible={false}
+      onPress={() => Keyboard.dismiss()}
+    >
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        <View style={styles.container}>
+          <View
+            style={{
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "green",
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 30,
+                fontFamily: "Kollektif",
+                textAlign: "left",
+              }}
             >
-                <View style={styles.container}>
-                    <View style={{ flex: 1, alignItems: "flex-start", justifyContent: 'center', backgroundColor: 'green' }}>
-                        <Text style={{ fontSize: 30, fontFamily: "Kollektif", }}>Email</Text>
-                        <View style={{ alignItems: "center", justifyContent: 'center' }}>
-                            <Input
-                                placeholder="Email"
-                                placeholderTextColor="gray"
-                                onChangeText={email => setEmail(email)}
-                                style={styles.input1}
-                                autoCapitalize="none"
-                                color="black"
-                                fontSize={17}
-                                value={email}
-                            />
-                        </View>
-                        <View style={{ alignItems: "flex-start", marginTop: "2%", justifyContent: 'center' }}>
-                            <Text style={{ fontSize: 30, fontFamily: "Kollektif", left: 5 }}>Password</Text>
-                            <Input
-                                placeholder="Password"
-                                placeholderTextColor="gray"
-                                secureTextEntry={true}
-                                onChangeText={pass => setPass(pass)}
-                                color="black"
-                                fontSize={17}
-                                style={styles.input1}
-                                value={pass}
-                            />
-                        </View>
-                        <View style={{ width: windowWidth, alignItems: 'center', justifyContent: 'center' }}>
-                            {(email != '' && pass != "") &&
-                                <GradientButton
-                                    onPress={async () => {
-                                        try {
-                                            login(email, pass)
-                                        } catch (err) {
-                                            Alert.alert(err);
-                                        }
-                                    }}
-                                    style={{ width: "50%" }}
-                                    innerStyle={{ paddingVertical: 10 }}
-                                >
-                                    Login
-                        </GradientButton>
-                            }
-                        </View>
-                    </View>
-
-
-
-                </View>
-            </KeyboardAvoidingView>
-        </TouchableWithoutFeedback>
-    );
+              Email
+            </Text>
+            <View style={{ alignItems: "center" }}>
+              <Input
+                placeholder="Email"
+                placeholderTextColor="gray"
+                onChangeText={(email) => setEmail(email)}
+                style={styles.input1}
+                autoCapitalize="none"
+                color="black"
+                fontSize={17}
+                value={email}
+              />
+            </View>
+            <View
+              style={{
+                alignItems: "flex-start",
+                marginTop: "2%",
+                justifyContent: "center",
+              }}
+            >
+              <Text style={{ fontSize: 30, fontFamily: "Kollektif" }}>
+                Password
+              </Text>
+              <Input
+                placeholder="Password"
+                placeholderTextColor="gray"
+                secureTextEntry={true}
+                onChangeText={(pass) => setPass(pass)}
+                color="black"
+                fontSize={17}
+                style={styles.input1}
+                value={pass}
+              />
+            </View>
+            <View
+              style={{
+                width: windowWidth,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              {email != "" && pass != "" && (
+                <GradientButton
+                  onPress={async () => {
+                    try {
+                      login(email, pass);
+                    } catch (err) {
+                      Alert.alert(err);
+                    }
+                  }}
+                  style={{ width: "50%" }}
+                  innerStyle={{ paddingVertical: 10 }}
+                >
+                  Login
+                </GradientButton>
+              )}
+            </View>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
+  );
 };
 
-export default AuthenticateViewController
-
+export default AuthenticateViewController;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "blue",
-        alignItems: 'center'
+  container: {
+    flex: 1,
+    backgroundColor: "blue",
+    alignItems: "center",
+  },
+  button: {
+    // height: 37,
 
-    },
-    button: {
-
-        height: 37,
-
-        borderColor: "#f76f6d",
-        borderWidth: 2,
-        borderRadius: 15,
-
-    },
-    input: {
-        padding: 10,
-        width: '70%',
-        height: 45,
-        borderRadius: 25,
-    },
-    input1: {
-        width: '90%',
-        height: 45,
-        borderRadius: 15,
-        borderColor: "black",
-        borderWidth: 1,
-        backgroundColor: "#e3e3e3"
-    },
-    logo: {
-        height: "33%",
-        width: "50%",
-        resizeMode: 'cover',
-        position: "relative",
-    },
+    borderColor: "#f76f6d",
+    borderWidth: 2,
+    borderRadius: 15,
+  },
+  input: {
+    // padding: 10,
+    // width: '70%',
+    // height: 45,
+    // borderRadius: 25,
+  },
+  input1: {
+    width: "90%",
+    height: 45,
+    borderRadius: 15,
+    borderColor: "black",
+    borderWidth: 1,
+    backgroundColor: "#e3e3e3",
+  },
+  logo: {
+    height: "33%",
+    width: "50%",
+    resizeMode: "cover",
+    position: "relative",
+  },
 });
